bb9377814c7e1cb7fa4f606df50ebcb8119a93fa
tobi <tobi_h@outlook.com>
2025-12-21 22:16:05 +0000

    signal kill update

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -14,16 +14,15 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	// "github.com/hashicorp/terraform-plugin-log/tflog"
-	// "fmt"
-	// "os"
-	// "os/exec"
-	// "os/signal"
-	// "syscall"
-	// "time"
-	// "google.golang.org/grpc"
-	// "google.golang.org/grpc/credentials/insecure"
-	// pb "github.com/Proxmox-Cloud/terraform-provider-pxc/internal/provider/protos"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"time"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "github.com/Proxmox-Cloud/terraform-provider-pxc/internal/provider/protos"
 )
 
 // Ensure PxcProvider satisfies various provider interfaces.
@@ -38,12 +37,14 @@ type PxcProvider struct {
 	// provider is built and ran locally, and "test" when running acceptance
 	// testing.
 	version string
+	exitCh chan bool
 }
 
 // PxcProviderModel describes the provider data model.
 type PxcProviderModel struct {
 	TargetPve    types.String `tfsdk:"target_pve"`
 	K8sStackName types.String `tfsdk:"k8s_stack_name"`
+	exitCh chan bool
 }
 
 func (p *PxcProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
@@ -81,6 +82,87 @@ func (p *PxcProvider) Configure(ctx context.Context, req provider.ConfigureReque
 	resp.ResourceData = data
 	resp.EphemeralResourceData = data
 
+	// launch our python grpc server
+
+	// todo: implement option to specify pythonpath in provider and pass that up here somehow
+	// or find a better solution
+	virtualEnv := os.Getenv("VIRTUAL_ENV")
+	if virtualEnv == "" {
+		resp.Diagnostics.AddError("Client Error", "VIRTUAL_ENV not defined, cant launch gprc")
+		return
+	}
+
+	// with this env var we can determine if we are running in a pytest context
+	pytestCurrent := os.Getenv("PYTEST_CURRENT_TEST")
+
+	// only install the pypi package if not in e2e scenario (in this case its installed via pip -e .)
+	if pytestCurrent == "" && p.version != "dev" {
+		// package will be published to pypi with same version tag as provider
+		// todo: check against installed version and prevent from removing / missmatching
+		pipCmd := exec.Command(fmt.Sprintf("%s/bin/pip", virtualEnv), "install", fmt.Sprintf("rpyc-pve-cloud==%s", p.version))
+
+		output, err := pipCmd.CombinedOutput()
+		if err != nil {
+			resp.Diagnostics.AddError("Could not launch rpc server", fmt.Sprintf("Command failed with error: %v - %s", err, string(output)))
+			return
+		}
+	}
+
+	// start pyhon grpc server as daemon
+	tflog.Info(ctx, fmt.Sprintf("Launching python rpc server on unix:///tmp/pc-rpc-%d.sock", os.Getpid()))
+	cmd := exec.Command(fmt.Sprintf("%s/bin/pcrpc", virtualEnv), strconv.Itoa(os.Getpid()))
+	if err := cmd.Start(); err != nil {
+		resp.Diagnostics.AddError("Failed to start Python backend", err.Error())
+		return
+	}
+
+	// wait for rpc to come up and healthcheck to succeed
+	deadline := time.Now().Add(10 * time.Second)
+
+	for {
+		if time.Now().After(deadline) {
+			resp.Diagnostics.AddError("Failed to start Python backend", "Deadline exceeded")
+			return
+		}
+
+		// try connect via grpc and health check
+		conn, err := grpc.NewClient(
+			fmt.Sprintf("unix:///tmp/pc-rpc-%d.sock", os.Getpid()),
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+		)
+		if err != nil {
+			time.Sleep(200 * time.Millisecond)
+			continue
+		}
+		defer conn.Close()
+
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+
+		healthClient := pb.NewHealthClient(conn)
+		hresp, err := healthClient.Check(ctx, &pb.HealthCheckRequest{})
+
+		if err != nil {
+			time.Sleep(200 * time.Millisecond)
+			continue
+		}
+
+		if hresp.Status != pb.HealthCheckResponse_SERVING {
+			time.Sleep(200 * time.Millisecond)
+			continue
+		}
+
+		break // its up and running
+	}
+
+	// launch routine to kill the server
+	go func(){
+		<-p.exitCh // wait for exit signal
+		
+		cmd.Process.Kill() // kill
+
+		p.exitCh <- true // call finished
+	}()
 }
 
 func (p *PxcProvider) Resources(ctx context.Context) []func() resource.Resource {
@@ -113,10 +195,11 @@ func (p *PxcProvider) Actions(ctx context.Context) []func() action.Action {
 	return []func() action.Action{}
 }
 
-func New(version string) func() provider.Provider {
+func New(version string, exitCh chan bool) func() provider.Provider {
 	return func() provider.Provider {
 		return &PxcProvider{
 			version: version,
+			exitCh: exitCh,
 		}
 	}
 }
--- main.go
+++ main.go
@@ -10,17 +10,6 @@ import (
 
 	"github.com/Proxmox-Cloud/terraform-provider-pxc/internal/provider"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-
-	"fmt"
-	"os"
-	"os/exec"
-	"time"
-
-	"strconv"
-
-	pb "github.com/Proxmox-Cloud/terraform-provider-pxc/internal/provider/protos"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
@@ -32,80 +21,27 @@ var (
 	// https://goreleaser.com/cookbooks/using-main.version/
 )
 
-// we cant do any printing in this function because that messes with the provider schema
-// initialization. but we still want to hook into the main process for starting and
-// killing our python grpc server daemon
-func startPythonGrpc() *exec.Cmd {
+// util for debugging
+// func debugFile(message string) {
 
-	// todo: implement option to specify pythonpath in provider and pass that up here somehow
-	// or find a better solution
-	virtualEnv := os.Getenv("VIRTUAL_ENV")
-	if virtualEnv == "" {
-		log.Fatal("Failed to start Python backend")
-	}
+// 	timestamp := time.Now().Format("2006-01-02_15-04-05.000")
 
-	// with this env var we can determine if we are running in a pytest context
-	pytestCurrent := os.Getenv("PYTEST_CURRENT_TEST")
+// 	// Create a filename with the timestamp
+// 	filename := fmt.Sprintf("/home/cloud/pve-cloud/terraform-pxc-backup/%s_%s.txt", message, timestamp)
 
-	// only install the pypi package if not in e2e scenario (in this case its installed via pip -e .)
-	if pytestCurrent == "" && version != "dev" {
-		// package will be published to pypi with same version tag as provider
-		// todo: check against installed version and prevent from removing / missmatching
-		pipCmd := exec.Command(fmt.Sprintf("%s/bin/pip", virtualEnv), "install", fmt.Sprintf("rpyc-pve-cloud==%s", version))
+// 	file, err := os.Create(filename)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer file.Close()
 
-		output, err := pipCmd.CombinedOutput()
-		if err != nil {
-			log.Fatal(fmt.Sprintf("Command failed with error: %v", err), string(output))
-		}
-	}
+// 	// Write some content to the file
+// 	_, err = file.WriteString(message)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
 
-	// start pyhon grpc server as daemon
-	cmd := exec.Command(fmt.Sprintf("%s/bin/pcrpc", virtualEnv), strconv.Itoa(os.Getpid()))
-	if err := cmd.Start(); err != nil {
-		log.Fatal("Failed to start Python backend", err.Error())
-	}
-
-	// wait for rpc to come up and healthcheck to succeed
-	deadline := time.Now().Add(10 * time.Second)
-
-	for {
-		if time.Now().After(deadline) {
-			log.Fatal("Failed to start Python backend - Deadline exceeded")
-			return nil
-		}
-
-		// try connect via grpc and health check
-		conn, err := grpc.NewClient(
-			fmt.Sprintf("unix:///tmp/pc-rpc-%d.sock", os.Getpid()),
-			grpc.WithTransportCredentials(insecure.NewCredentials()),
-		)
-		if err != nil {
-			time.Sleep(200 * time.Millisecond)
-			continue
-		}
-		defer conn.Close()
-
-		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-		defer cancel()
-
-		healthClient := pb.NewHealthClient(conn)
-		hresp, err := healthClient.Check(ctx, &pb.HealthCheckRequest{})
-
-		if err != nil {
-			time.Sleep(200 * time.Millisecond)
-			continue
-		}
-
-		if hresp.Status != pb.HealthCheckResponse_SERVING {
-			time.Sleep(200 * time.Millisecond)
-			continue
-		}
-
-		break // its up and running
-	}
-
-	return cmd
-}
 
 func main() {
 	var debug bool
@@ -121,18 +57,9 @@ func main() {
 		Debug:   debug,
 	}
 
-	//start our python rpc server here
-	// cmd := startPythonGrpc()
-
-	// // Ensure cleanup of the Python server even if Serve panics or exits unexpectedly
-	// defer func() {
-	// 	// Kill the Python server after Serve finishes (even if it panics)
-	// 	if cerr := cmd.Process.Kill(); cerr != nil {
-	// 		log.Printf("Failed to kill Python backend: %v", cerr)
-	// 	}
-	// }()
-
-	err := providerserver.Serve(context.Background(), provider.New(version), opts)
+	exitCh := make(chan bool)
+	
+	err := providerserver.Serve(context.Background(), provider.New(version, exitCh), opts)
 
 	if err != nil {
 		log.Fatal(err.Error())
